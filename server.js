2833289cba01bd038e60376a12276cc6824344b0
Himanshu Sharma <jalotrahimanshu13@gmail.com>
2023-03-25 23:07:30 +0530

    basic routing added

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+app.get('/cards',(req,res)=>{
+    res.send("Cards data fetched from Mongodb")
+})
+app.get('/experts',(req,res)=>{
+    res.send("Experts data fetched from Mongodb ")
+})
+
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
