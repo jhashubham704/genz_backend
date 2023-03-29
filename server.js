5f7a2175e2594ade04696defd3853c745a6cdf5e
Himanshu Sharma <jalotrahimanshu13@gmail.com>
2023-03-29 22:51:04 +0530

    cards and experts api created

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
-app.use(express.json());
+const cardModel = require('./models/card.model');
+const expertModel = require('./models/expert.model');
+var cors = require('cors')
+app.use(cors())
+
+mongoose.connect("mongodb+srv://himanshu:123@cluster0.8mf1qpv.mongodb.net/genzdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const database = mongoose.connection;
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
+    
 })
-app.get('/cards',(req,res)=>{
-    res.send("Cards data fetched from Mongodb")
+app.get('/cards', async (req, res) => {
+  try{
+      const data = await cardModel.find();
+      res.json(data)
+  }
+  catch(error){
+      res.status(500).json({message: error.message})
+  }
 })
-app.get('/experts',(req,res)=>{
-    res.send("Experts data fetched from Mongodb ")
+
+app.get('/experts',async(req,res)=>{
+  try{
+    const data = await expertModel.find({isAdmin:true});
+    console.log(data);
+    
+      res.send(data)
+}
+catch(error){
+    res.status(500).json({message: error.message})
+}
 })
 
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+app.listen(8085, () => {
+    console.log(`Server Started at ${8085}`)
 })
